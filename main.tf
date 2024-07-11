c8fcfddebda08303f3e9c9e29fb0161d44987363
geesaladurgaakhil <56544604+geesaladurgaakhil@users.noreply.github.com>
2024-07-11 07:06:15 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+# You only need to run this once per subscription
+# tfsec
+terraform {
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.60.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "env" {
+  #source     = "git::https://github.com/DigitalInnovation/terraform-mns-environment-names?ref=v0.1.0"
+  short_name = local.subscription_type
+}
+
